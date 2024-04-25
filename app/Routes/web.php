fd30c182ef0934313a303113c5f74352f507eb81
Valdimir Barros de Carvalho <valdimirbarrosti@gmail.com>
2024-04-25 20:59:30 -0300

    cleaning

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -5,5 +5,4 @@ use App\Controllers\UserController;
 
 
 $this->router->get('/', [HomeController::class, 'index']);
-$this->router->get('/users', [UserController::class, 'index']);
-$this->router->get('/users/123', [UserController::class, 'show']);
+
